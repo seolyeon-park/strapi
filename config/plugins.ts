3b367cc108da003abee8f18e48fd3b2cd901312c
sanchoiworks <sanecdoteofficial@gmail.com>
2025-05-27 14:42:03 +0900

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -11,6 +11,7 @@ export default ({ env }) => ({
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
+        secure: true
       },
     },
   },
