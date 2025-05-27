76b4e470c7ec18bdbc034289dad5d9a0f4164f1a
sanchoiworks <sanecdoteofficial@gmail.com>
2025-05-27 17:55:28 +0900

    파비콘

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -7,8 +7,8 @@ export default [
         useDefaults: true,
         directives: {
           'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'https://res.cloudinary.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'https://res.cloudinary.com'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
       },
